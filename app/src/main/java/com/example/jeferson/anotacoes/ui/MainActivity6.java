fe5425e0de8011c458f2f934b9a5b2c84287877b
jeferson <jefersondeoliveiralopes@gmail.com>
2016-09-14 16:36:11 -0300

    add ArrayAdapter

---
+++ app/src/main/java/com/example/jeferson/anotacoes/ui/MainActivity6.java
@@ -0,0 +1,57 @@
+package com.example.jeferson.anotacoes.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.example.jeferson.anotacoes.R;
+import com.example.jeferson.anotacoes.data.Anotacao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity6 extends AppCompatActivity {
+
+    private EditText editBox;
+    private Button insertButton;
+    private ListView notesList;
+
+    private List<Anotacao> listAnotacao;
+    private ArrayAdapter<Anotacao> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_2);
+
+        editBox = (EditText) findViewById(R.id.edit_box);
+        insertButton = (Button) findViewById(R.id.insert_button);
+        notesList = (ListView) findViewById(R.id.notes_list);
+
+        listAnotacao = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listAnotacao);
+        notesList.setAdapter(adapter);
+
+        insertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String texto = editBox.getText().toString();
+
+                Anotacao anotacao = new Anotacao();
+                anotacao.setDescricao(texto);
+
+                listAnotacao.add(anotacao);
+
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+
+    }
+
+}
